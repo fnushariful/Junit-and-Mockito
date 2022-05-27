4e75c5d158cbe6fb334f797da8fe6f17a2d721bf
FNU Shariful <robinmbstu12@gmail.com>
2022-05-27 15:01:13 -0400

    Test annotation and assertion has been checked successfully.

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void test() {
+//        System.out.println("Hello World");
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected,actual);
+    }
+}
